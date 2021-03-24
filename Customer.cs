9dd3d94bc584e5b5d184299107d353a3a487eb55
Teslime <teslime_mlpnr@outlook.com>
2021-03-24 14:45:30 +0300

    Added 'if ' block for 0. reservation. -Customer class

--- Customer.cs
+++ Customer.cs
@@ -25,7 +25,10 @@ namespace hotel
         private string reservationSelection(int reservationNo)
         {
             string typeMessage="";
-           
+            if(reservationNo == 0)
+            {
+                typeMessage = "The full fee is paid before the last 90 days.\n 75 % of the base price is paid.\n There is no refund. \n Change on the day of reservation takes place with an additional fee.\n The additional fee is 110 % of the base price.";
+            }
             return typeMessage;
         }
     }
