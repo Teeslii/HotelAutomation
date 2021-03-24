bcbcd2ceae4263478d501f25dd3246edefe06e7f
Teslime <teslime_mlpnr@outlook.com>
2021-03-24 14:47:58 +0300

    Added 'if ' block for 1. reservation. - Customer class

--- Customer.cs
+++ Customer.cs
@@ -29,6 +29,10 @@ namespace hotel
             {
                 typeMessage = "The full fee is paid before the last 90 days.\n 75 % of the base price is paid.\n There is no refund. \n Change on the day of reservation takes place with an additional fee.\n The additional fee is 110 % of the base price.";
             }
+            else if(reservationNo == 1)
+            {
+                typeMessage = "We will e - mail 45 days prior to payment.\nPayment is made 15 days before booking.\nIf payment is not made, the reservation will be canceled.\nIt pays 85 % of the base price.\nThere is no refund.\nChange on the day of reservation takes place with an additional fee. \nThe surcharge price is 110 % of the base price.\nIf it is lower than the old wage, the remaining amount is not paid.\n";
+            }
             return typeMessage;
         }
     }
