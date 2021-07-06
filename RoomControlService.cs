21e08d20c649f467a233ecf4c780a69d4a60096a
Teslime <teslime_mlpnr@outlook.com>
2021-07-06 21:26:58 +0300

    RoomColor has been deleted from the query sentence of the insertRoom variable in the AddingRoom() method. -RoomControlService class

--- RoomControlService.cs
+++ RoomControlService.cs
@@ -18,7 +18,7 @@ namespace hotel
             using (var connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
-                string insertRoom = "Insert into Room(ID, roomNo, roomColor, checkIn) values ( @ID, @roomNo, 'Salmon', GETDATE())";
+                string insertRoom = "Insert into Room(ID, roomNo, checkIn) values ( @ID, @roomNo, GETDATE())";
                 SqlCommand Insert = new SqlCommand(insertRoom, connection);
                 Insert.Parameters.Add(new System.Data.SqlClient.SqlParameter("@ID", SqlDbType.Int) { Value = ID });
                 Insert.Parameters.Add(new System.Data.SqlClient.SqlParameter("@roomNo", SqlDbType.Int) { Value = RoomNo });
