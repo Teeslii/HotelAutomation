4eba20d0de83d6ea02849a40a0b22028ffb1a5bd
Teslime <teslime_mlpnr@outlook.com>
2021-08-06 22:41:19 +0300

    CustomerDto class has been created.

---
+++ Models/CustomerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel.Models
+{
+    public class CustomerDto
+    {
+
+    }
+}
