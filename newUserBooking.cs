6f847ddd3aaf7412c1e018067b0a80f784c7cf9e
Teslime <teslime_mlpnr@outlook.com>
2021-08-07 15:34:25 +0300

    In the MapperCustomerDto() method, the Customer class and the CustomerDto class are maked mapper process. - newUserBooking from

--- newUserBooking.cs
+++ newUserBooking.cs
@@ -30,9 +30,28 @@ namespace hotel
 
         private void MapperCustomerDto()
         {
+            customer.FirstName = txtFirstName.Text;
+            customer.LastName = txtLastName.Text;
+            long.TryParse(txtTelephone.Text, out long Telephone);
+            customer.Telephone = Telephone;
+            customer.Mail = txtMail.Text;
+            long.TryParse(txtID.Text, out long ID);
+            customer.TC = ID;
+            int.TryParse(txtPrice.Text, out int Price);
+            customer.Price = Price;
+            DateTime.TryParse(txtLoginDate.Text, out DateTime LoginDate);
+            customer.LoginDate = LoginDate;
+            DateTime.TryParse(txtExitDate.Text, out DateTime ExitDate);
+            customer.ExitDate = ExitDate;
+            customer.ReservationType = cBoxChoose.SelectedItem.ToString();
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Customer,CustomerDto>());
+            var mapper = new Mapper(config);
+            var customerDto = mapper.Map<CustomerDto>(customer);
+            
 
         }
-        
+
         private void txtExitDate_ValueChanged(object sender, EventArgs e)
         {
        
