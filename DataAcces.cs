dfb0c541184e1bb990e961daa1279c85dc77cd24
Teslime <teslime_mlpnr@outlook.com>
2021-08-07 14:56:46 +0300

    IDataAcces class implement  to DataAcces class.

--- DataAcces.cs
+++ DataAcces.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hotel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,11 @@ using System.Threading.Tasks;
 
 namespace hotel
 {
-    public class DataAcces
+    public class DataAcces : IDataAcces
     {
-
+        public void SaveCustomer(CustomerDto customerDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
