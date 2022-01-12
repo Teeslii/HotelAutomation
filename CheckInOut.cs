23f80536e98cd7a504d466ae8e6122ad7cfa58ae
Teslime <teslime_mlpnr@outlook.com>
2022-01-12 18:00:45 +0300

    Deleted have been textbox, label and related methods that will not use from CheckInOut form.

--- CheckInOut.cs
+++ CheckInOut.cs
@@ -20,41 +20,15 @@ namespace hotel
             InitializeComponent();
            
         }
-        private string ConnectionString = ConfigurationManager.ConnectionStrings["hotel.Properties.Settings.Setting"].ConnectionString;
+        
 
         public void TransferId(int SaveCustomerId)
         {
             roomControlCheck.CustomerId = SaveCustomerId;
         }
-        private int convertID;
        
-        private void btnResearch_Click(object sender, EventArgs e)
-        {
-            convertID = Convert.ToInt32(txtResearchRoom.Text);
-
-            using (var connection = new SqlConnection(ConnectionString))
-            {  
-                connection.Open();
-                string ResearchChart = " select firstName, lastName, telephone, price, howManyDay, Room.roomNo from customer inner join Room  on customer.ID = Room.ID where Room.roomNo= @convertID ";
-                SqlCommand sqlCommand = new SqlCommand(ResearchChart, connection);
-                sqlCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@convertID", SqlDbType.Int, 3) { Value = convertID });
-                sqlCommand.ExecuteNonQuery();
-                SqlDataReader readerInOut =  sqlCommand.ExecuteReader();
-                while (readerInOut.Read())
-                {
-                    txtFirstName.Text = readerInOut["firstName"].ToString();
-                    txtLastName.Text = readerInOut["lastName"].ToString();
-                    txtTelephone.Text = readerInOut["telephone"].ToString();
-
-                    txtPrice.Text = readerInOut["price"].ToString();
-                    txthowManyDay.Text = readerInOut["howManyDay"].ToString();
-                    txtRoomNo.Text = readerInOut["roomNo"].ToString();
-                }
-                readerInOut.Close();
-                connection.Close();
-            }
-             
-        }
+       
+       
 
         private void btnhomeback_Click(object sender, EventArgs e)
         {
