2780c361b8e09ac346d2d76723db24a70c263d6d
Teslime <teslime_mlpnr@outlook.com>
2022-01-12 12:37:40 +0300

    deleted   CustomerDto class.

--- SqlDataAccess.cs
+++ SqlDataAccess.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using hotel.Models;
 using System.Configuration;
 using System.Data;
 using System.Data.Sql;
 using System.Data.SqlClient;
-using AutoMapper;
+ 
 
 namespace hotel
 {
