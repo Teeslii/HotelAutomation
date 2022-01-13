05d32aa805597a151f97b22bc971aab321e5d3f4
Teslime <teslime_mlpnr@outlook.com>
2022-01-13 17:56:05 +0300

    if blocks, have been deleted because they aren't comprehensive.

--- RoomControl.cs
+++ RoomControl.cs
@@ -76,134 +76,52 @@ namespace hotel
 
         }
      
-        public int CustomerId { get; set; }
+         
         
         private void Room1_Click(object sender, EventArgs e)
         {
-           
-            if (Room1.BackColor == Color.Lime)
-            {
-                RoomControlService.AddingRoom(1, CustomerId);
-                Room1.BackColor = Color.Salmon;
-            }
-            else
-            {
-                RoomControlService.DeleteRoom(1);
-                Room1.BackColor = Color.Lime;
-            }
+            
         }
 
         private void Room2_Click(object sender, EventArgs e)
         {
-            if (Room2.BackColor == Color.Lime)
-            {
-                RoomControlService.AddingRoom(2, CustomerId);
-                Room2.BackColor = Color.Salmon;
-            }
-            else
-            {
-                RoomControlService.DeleteRoom(2);
-                Room2.BackColor = Color.Lime;
-            }
+            
         }
 
         private void Room3_Click(object sender, EventArgs e)
         {
-            if (Room3.BackColor == Color.Lime)
-            {
-                RoomControlService.AddingRoom(3, CustomerId);
-                Room3.BackColor = Color.Salmon;
-            }
-            else
-            {
-                RoomControlService.DeleteRoom(3);
-                Room3.BackColor = Color.Lime;
-            }
+            
         }
 
         private void Room4_Click(object sender, EventArgs e)
         {
 
-            if (Room4.BackColor == Color.Lime)
-            {
-                RoomControlService.AddingRoom(4, CustomerId);
-                Room4.BackColor = Color.Salmon;
-            }
-            else
-            {
-                RoomControlService.DeleteRoom(4);
-                Room4.BackColor = Color.Lime;
-            }
+            
         }
 
         private void Room5_Click(object sender, EventArgs e)
         {
-            if (Room5.BackColor == Color.Lime)
-            {
-                RoomControlService.AddingRoom(5, CustomerId);
-                Room5.BackColor = Color.Salmon;
-            }
-            else
-            {
-                RoomControlService.DeleteRoom(5);
-                Room5.BackColor = Color.Lime;
-            }
+            
         }
 
         private void Room6_Click(object sender, EventArgs e)
         {
-            if (Room6.BackColor == Color.Lime)
-            {
-                RoomControlService.AddingRoom(6, CustomerId);
-                Room6.BackColor = Color.Salmon;
-            }
-            else
-            {
-                RoomControlService.DeleteRoom(6);
-                Room6.BackColor = Color.Lime;
-            }
+             
         }
 
         private void Room7_Click(object sender, EventArgs e)
         {
-            if (Room7.BackColor == Color.Lime)
-            {
-                RoomControlService.AddingRoom(7, CustomerId);
-                Room7.BackColor = Color.Salmon;
-            }
-            else
-            {
-                RoomControlService.DeleteRoom(7);
-                Room7.BackColor = Color.Lime;
-            }
+             
         }
 
         private void Room8_Click(object sender, EventArgs e)
         {
-            if (Room8.BackColor == Color.Lime)
-            {
-                RoomControlService.AddingRoom(8, CustomerId);
-                Room8.BackColor = Color.Salmon;
-            }
-            else
-            {
-                RoomControlService.DeleteRoom(8);
-                Room8.BackColor = Color.Lime;
-            }
+             
         }
 
         private void Room9_Click(object sender, EventArgs e)
         {
-            if (Room9.BackColor == Color.Lime)
-            {
-                RoomControlService.AddingRoom(9, CustomerId);
-                Room9.BackColor = Color.Salmon;
-            }
-            else
-            {
-                RoomControlService.DeleteRoom(9);
-                Room9.BackColor = Color.Lime;
-            }
+             
         }
     }
 }
