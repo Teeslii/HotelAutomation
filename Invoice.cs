023292a4bc0f93db2af4906b3f597838a09f799c
Teslime <teslime_mlpnr@outlook.com>
2022-02-15 14:54:54 +0300

    Removed database connections because they cannot be made inside the form.

--- Invoice.cs
+++ Invoice.cs
@@ -8,8 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Configuration;
-using System.Data.Sql;
-using System.Data.SqlClient;
+ 
 
 namespace hotel
 {
@@ -20,8 +19,7 @@ namespace hotel
             InitializeComponent();
         }
       
-        SqlConnection connectionString = new SqlConnection(ConfigurationManager.ConnectionStrings["hotel.Properties.Settings.Setting"].ConnectionString);
-      
+         
         private int RoomNoInvoice;
         private void cbFeePayable_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -47,18 +45,6 @@ namespace hotel
         private void btnResearch_Click(object sender, EventArgs e)
         {
             
-            connectionString.Open();
-            SqlCommand roomFee = new SqlCommand("select price from customer join Room on customer.ID = Room.ID where Room.roomNo= @roomNo", connectionString);
-            roomFee.Parameters.Add(new System.Data.SqlClient.SqlParameter("@roomNo", SqlDbType.Int, 5) { Value = RoomNoInvoice });
-            roomFee.ExecuteNonQuery();
-            SqlDataReader dataReader = roomFee.ExecuteReader();
-
-            while(dataReader.Read())
-            {
-               lblPaymentAmountText.Text = dataReader["price"].ToString();
-            }
-            dataReader.Close();
-            connectionString.Close();
         }
 
         private void btnPaid_Click(object sender, EventArgs e)
