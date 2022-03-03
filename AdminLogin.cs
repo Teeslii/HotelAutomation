d36292d0d1aa0b9802938d1e1dae20d641b131db
Teslime <teslime_mlpnr@outlook.com>
2022-03-03 13:55:20 +0300

    Added process for verifying user login.

--- AdminLogin.cs
+++ AdminLogin.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+
 
 namespace hotel
 {
@@ -16,12 +18,45 @@ namespace hotel
         {
             InitializeComponent();
         }
+        
+        public void VerifyUserLogin()
+        {
+            string _userName = userNameTxt.Text.Trim();
+            string _password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(userNameTxt.Text))
+                throw new ArgumentException("User Name: ");
+
+            if (string.IsNullOrEmpty(txtPassword.Text))
+                throw new ArgumentException("Password: ");
+
+            SHA1 sha = new SHA1CryptoServiceProvider();
+
+            byte[] bytes  = sha.ComputeHash(Encoding.UTF8.GetBytes(_password));
+
+            StringBuilder builder = new StringBuilder();
+            foreach(var item in bytes)
+            {
+                builder.Append(item.ToString("x2"));
+            }
+            bool ResultVerification = AdminLoginService.VerificationUser(builder.ToString(), _userName);
+
+            if (ResultVerification == true)
+            {
+                MessageBox.Show("Successfully logged in.");
+                MainPage mainPage = new MainPage();
+                mainPage.Show();
+                this.Hide();
+            }
+
+            else
+                MessageBox.Show("Failed to login.");
+        }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            MainPage mainPage = new MainPage();
-            mainPage.Show();
-            this.Hide();
+            VerifyUserLogin();
+          
         }
     }
 }
---
+++ AdminLoginService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace hotel
+{
+   public static class AdminLoginService
+    {
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["hotel.Properties.Settings.Setting"].ConnectionString;
+
+       
+
+        public static bool VerificationUser(string _convertedPassword, string _userName)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+         
+                bool _resultVerication;
+                var verificationQuery = "SELECT Password from Employee where UserName = @UserName";
+                var cmd = new SqlCommand(verificationQuery, connection);
+                cmd.Parameters.Add(new System.Data.SqlClient.SqlParameter("@UserName", SqlDbType.NVarChar, 150) { Value = _userName});
+
+                var _savedPassword = cmd.ExecuteScalar();
+
+
+                if (_convertedPassword == _savedPassword.ToString())
+                {
+                    _resultVerication = true;
+                }
+                else
+                {
+                    _resultVerication = false;
+                }
+                return _resultVerication;
+
+                connection.Close();
+            }
+        }
+    }
+}
